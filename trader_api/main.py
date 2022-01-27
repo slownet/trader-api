9a22a3030a98f0d3f341557cc4ad6dc7ba30ec1f
Maxim Bilan <bilan@europe.com>
2022-01-27 17:06:12 +0100

    Update stock endpoint controllers. Fix main.py imports. Add yfinance.

--- trader_api/main.py
+++ trader_api/main.py
@@ -1,9 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 
-from .internal import ping
-from .routers import stocks
+from trader_api.internal import ping
+from trader_api.routers import stocks
 
 app = FastAPI()
 
 app.include_router(ping.router)
 app.include_router(stocks.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app)
--- trader_api/routers/stocks.py
+++ trader_api/routers/stocks.py
@@ -1,3 +1,4 @@
+import yfinance as yf
 from fastapi import APIRouter
 
 router = APIRouter(
@@ -8,3 +9,19 @@ router = APIRouter(
 @router.get('/{ticker}')
 def read_stocks_ticker(ticker: str):
     return {'result': f'Your ticker sir: {ticker}'}
+
+
+@router.get('/{ticker}/info')
+def read_stocks_ticker(ticker: str):
+    """Returns a stock information."""
+    ticker_data = yf.Ticker(ticker)
+    return {'result': ticker_data.info}
+
+
+@router.get('/{ticker}/owners')
+def read_stocks_owners(ticker: str):
+    """Returns a list of gamers who own such stock."""
+
+    # TODO: add database requests.
+
+    return {'result': []}
